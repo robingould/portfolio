ca7a69675bce2c303dc7ea6d164056bdd0b81eca
plantfriendzx <froygyg@gmail.com>
2023-03-15 14:21:53 -0400

    3

--- codes/error.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <title>Error ! lol ! idk what happend XDd !</title>
-            </head>
-                <body>
-                    <?php echo '<p>I-I uhh.. sorry.. I lost the webpsite,... ;_; 404 error or something ?idk what its called :P</p>'; ?>
-                    </body>
-</html>

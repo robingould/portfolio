a2b1f512372205a95cfb6fefd69e0c1bd9edc949
plantfriendzx <froygyg@gmail.com>
2023-03-24 17:02:14 -0400

    z

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         neoc: resolve(__dirname, 'neoc.html'),
+        cloudynite: resolve(__dirname, 'cloudynite.gif'),
+        oneko: resolve(__dirname, 'oneko.gif'),
+        sheep: resolve(__dirname, 'sheep.gif'),
         codes: resolve(__dirname, 'codes/index.html'),
         success: resolve(__dirname, 'success.html'),
         final: resolve(__dirname, 'final.html'),

b2d0c9a7861c60c2d3c886d0f54621553bc00433
plantfriendzx <robin.gould@uconn.edu>
2023-09-14 13:58:51 -0400

    2 is done, but cat is not

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -121,7 +121,7 @@ export default class Controls{
                     }
                  });
                  this.secondMoveTimeline.to(this.camera.perspectiveCamera.position, {
-                    x: 4.2,
+                    x: 4.15,
                     y: () => {
                         return -0.3;
                     },

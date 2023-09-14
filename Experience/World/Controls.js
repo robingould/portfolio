4ffbb37e59c1bcc506ae7bcce56bcfcd3d1ab5e4
plantfriendzx <robin.gould@uconn.edu>
2023-09-14 13:40:37 -0400

    cat

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -168,7 +168,7 @@ export default class Controls{
                  this.fourthMoveTimeline.to(this.luckycat.position, {
                     x: -3.2,
                     y: 4.8,
-                    z: 2
+                    z: 2.2
                  },"samed" 
                  );
                  this.fourthMoveTimeline.to(this.luckycat.rotation, {

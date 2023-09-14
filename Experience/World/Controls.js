cad41939cf3f69e1895c887b87d0447b1a726437
plantfriendzx <robin.gould@uconn.edu>
2023-09-14 14:03:13 -0400

    overshot

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -125,7 +125,7 @@ export default class Controls{
                     y: () => {
                         return -0.3;
                     },
-                    z: 2.6,
+                    z: 2.56,
                  },"sameb"
                  );
                  this.secondMoveTimeline.to(this.camera.perspectiveCamera.rotation, {

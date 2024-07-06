ad3341dbd30ccb2a6a40de50cc1b62229e1ef657
plantfriendzx <froygyg@gmail.com>
2024-07-06 14:52:38 -0400

    lot of changes

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        lifestory: resolve(__dirname, 'lifestory.html'),
+        yap: resolve(__dirname, '/pages/yap/'),
+        about: resolve(__dirname, '/about/'),
+        projects: resolve(__dirname, '/projects/'),
         codes: resolve(__dirname, 'codes/index.html'),
         success: resolve(__dirname, 'success.html'),
         final: resolve(__dirname, 'final.html'),
         failure: resolve(__dirname, 'codes/failure.html'),
         error: resolve(__dirname, 'codes/error.html'),
+        music: resolve(__dirname, 'm.html')
       },
     },
   },
